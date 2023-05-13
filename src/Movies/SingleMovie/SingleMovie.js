e1e2c4589b540368ce28bbb8da3529c871a0a88e
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-05-12 21:11:23 -0400

    feature: Add display of single movie and allow for return to main

--- src/Movies/SingleMovie/SingleMovie.js
+++ src/Movies/SingleMovie/SingleMovie.js
@@ -2,31 +2,30 @@ import React from "react"
 import "../SingleMovie/SingleMovie.css"
 import DetailedView from "../../DetailedView/DetailedView"
 
-function SingleMovie ({selectedPoster}) {
-  const singleMovieDisplay = selectedPoster.map(detail => {
+function SingleMovie({ selectedPoster, resetMainPage }) {
+  const singleMovieDisplay = selectedPoster.map((detail) => {
     return (
       <DetailedView
-        id = {detail.id}
-        title = {detail.title}
-        posterImg = {detail.poster_path}
-        backdropImg = {detail.backdrop_path} 
-        releaseDate = {detail.release_date}
-        overview = {detail.overview}
-        averageRating = {detail.average_rating}  
-        genres = {detail.genres} 
-        budget = {detail.budget} 
-        revenue = {detail.revenue}
-        runtime = {detail.runtime} 
-        tagline = {detail.tagline} 
-        key = {detail.id}
-      />  
+        id={detail.id}
+        title={detail.title}
+        posterImg={detail.poster_path}
+        backdropImg={detail.backdrop_path}
+        releaseDate={detail.release_date}
+        overview={detail.overview}
+        averageRating={detail.average_rating}
+        genres={detail.genres}
+        budget={detail.budget}
+        revenue={detail.revenue}
+        runtime={detail.runtime}
+        tagline={detail.tagline}
+        key={detail.id}
+        resetMainPage={resetMainPage}
+      />
     )
   })
-    return (
-      <div className="detail-container">
-        {singleMovieDisplay}
-      </div>
-    )
+  return <div className="detail-container">
+    {singleMovieDisplay}
+    </div>
 }
 
 export default SingleMovie
