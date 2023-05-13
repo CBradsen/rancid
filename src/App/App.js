160ad3c2001c05264b0423cea27478b47efca34d
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-05-13 14:18:07 -0400

    feature: Establish API call to retrieve data

--- src/App/App.js
+++ src/App/App.js
@@ -5,47 +5,51 @@ import MainMovies from '../Movies/MainMovies/MainMovies';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SingleMovie from '../Movies/SingleMovie/SingleMovie';
+import { fetchData } from '../apiCalls';
 
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      posters: movieData.movies,
-      selectedPoster: []
+      posters: [],
+      selectedPoster: [],
+      error:''
     }
   }
 
-    // componentDidMount() {
-    //   this.getMovieData();
-    // }
-
-    // getMovieData() {
-    //  const displayPosters = this.setState({posters: movieData.movies})
-    //  return console.log(movieData)
-    //   // We need to pull in the data from movieData 
-    //   // and then setState 
-    //   //use it in the Movies folder to have in show on the UI
-    // }
-
-    setSinglePoster = (posterId) => {
-      const clickedPoster = this.state.posters.find((poster) => poster.id === posterId);
-      this.setState((prevState) => ({
-        selectedPoster: [...prevState.selectedPoster, clickedPoster],
-      }));
-    };
-
-    resetMainPage = () => {
-      this.setState({selectedPoster: []})
+    componentDidMount() {
+    this.getMovieData()
     }
+
+    getMovieData() {
+      fetchData()    
+      .then(jsonData => {
+        console.log(jsonData)
+        this.setState({ posters: jsonData });
+      })
+      .catch(error => this.setState({error: error.message}))
+  }
+
+    // setSinglePoster = (posterId) => {
+    //   const clickedPoster = this.state.posters.find((poster) => poster.id === posterId);
+    //   this.setState((prevState) => ({
+    //     selectedPoster: [...prevState.selectedPoster, clickedPoster],
+    //   }));
+    // };
+
+    // resetMainPage = () => {
+    //   this.setState({selectedPoster: []})
+    // }
 
 
     render() {
       return (
         <main className="App">
           <Header />
-          {this.state.selectedPoster.length ? (<SingleMovie selectedPoster={this.state.selectedPoster} resetMainPage={this.resetMainPage} />) :
-          (<MainMovies posters={this.state.posters} setSinglePoster={this.setSinglePoster}/>)}
+          {/* {this.state.selectedPoster.length ? (<SingleMovie selectedPoster={this.state.selectedPoster} resetMainPage={this.resetMainPage} />) :
+          (<MainMovies posters={this.state.posters} setSinglePoster={this.setSinglePoster}/>)} */}
+          <MainMovies posters={this.state.posters} setSinglePoster={this.setSinglePoster}/>
           <Footer />
         </main>
       );
