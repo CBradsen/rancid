0286d3f82c3360f1ef68174ef300281e78f59f85
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-05-13 18:44:37 -0400

    fix: Repaired bug that was preventing errors from displaying for user

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ const fetchData = () => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies`)
   .then(res => {
     if(!res.ok){
-      throw new Error (`Please try again there is an error ${res.status}`)
+      throw new Error(`Please try again there is an error ${res.status}`)
     } else {
       return res.json()
     }
@@ -13,7 +13,7 @@ const specficData = (id) => {
   return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
   .then(res => {
     if(!res.ok){
-      throw new Error (`Please try again there is an error ${res.status}`)
+      throw new Error(`Please try again there is an error ${res.status}`)
     } else {
       return res.json()
     }
