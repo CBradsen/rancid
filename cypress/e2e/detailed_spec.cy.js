730d412d2aaabcceb7e57dcce0741309372915a4
CBradsen <117617970+CBradsen@users.noreply.github.com>
2023-05-17 07:53:06 -0600

    feature: Begin cypress tests on detailed view

--- cypress/e2e/detailed_spec.cy.js
+++ cypress/e2e/detailed_spec.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe("Detailed page", function () {
+  beforeEach(() => {
+  cy.fixture("movieData.json").as("movieData")
+  .intercept("GET", "https://rancid-tomatillos.herokuapp.com/api/v2/movies", {
+    statusCode: 200,
+    fixture: "movieData"
   })
+  .visit("http://localhost:3000/")
+  .then(() => {
+    cy.get(".poster-img").first().click()
+  })
+});
+
+  it("should allow the user to click a movie poster and be taken to a page showing details about the movie they clicked", function() {
+    cy.get(".single-movie")
+    cy.get(".backdrop").should("be.visible");
+    cy.get(".single-poster").should("be.visible");
+  });
+
+  it("should allow the user to click a button to return to the main page", function() {
+    
+    
+  })
+
+  
+
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App/App';
+import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from './Etc/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +10,10 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+ 
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
